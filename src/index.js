bf7bef0990a738b5b4406b3d81d0d3259a9ca4fa
josuemartel <josueadolfo@gmail.com>
2018-07-20 01:59:21 -0600

    final

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var boldElement = <b>Text (as a string)</b>;
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
