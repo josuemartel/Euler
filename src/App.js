a5eaea7c01d50b08790e474feba1879c8251243e
josuemartel <josueadolfo@gmail.com>
2018-07-19 21:37:28 -0600

    Show the first panel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result3: 4
+    };
+    this.seeSolve = this.seeSolve.bind(this);
+  }
+  
+  seeSolve() {
+    this.setState({
+      result3: 8 })
+  }
+
   render(){
     return(
       <div className="app">
@@ -15,7 +28,8 @@ class App extends Component {
               <h2 className="problem_title">Multiples of 3 and 5</h2>
               <p className="problem_description">If we list all natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
               <p className="problem_Question">Find the sum of all the multiples of 3 or 5 below 1000.</p>
-              <p className="problem_btn">Solve</p>
+              <p className="problem_btn" onClick={this.seeSolve}>Solve</p>
+              <p className="problem_btn">{this.state.result3}</p>
             </article>
           </div>
           <div className="column">
@@ -31,10 +45,10 @@ class App extends Component {
               <h2 className="problem_title">Multiples of 3 and 5</h2>
               <p className="problem_description">If we list all natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
               <p className="problem_Question">Find the sum of all the multiples of 3 or 5 below 1000.</p>
-              <p className="problem_btn">Solve</p>
+              <button  className="problem_btn" onClick={this.seeSolve}>Solve</button >
             </article>
           </div>
-          <div className="column">
+          <div className="columnResult">
             <article className="problem">
               <h2 className="problem_title">Power digit sum</h2>
               <p className="problem_description">215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var boldElement = <b>Text (as a string)</b>;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
