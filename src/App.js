755ff91324d8b29c1cbf2a6ba0d39fbf93b4c545
josuemartel <josueadolfo@gmail.com>
2018-07-19 20:44:49 -0600

    change on app.css

--- src/App.js
+++ src/App.js
@@ -10,18 +10,38 @@ class App extends Component {
           <h1 className="App-title">Project Euler</h1>
         </header>
         <div className="container">
-          <article className="problem">
-            <h2 className="problem_title">Multiples of 3 and 5</h2>
-            <p className="problem_description">If we list all natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
-            <p className="problem_Question">Find the sum of all the multiples of 3 or 5 below 1000.</p>
-            <p className="problem_btn">Solution</p>
-          </article>
-          <article className="problem">
-            <h2 className="problem_title">Power digit sum</h2>
-            <p className="problem_description">215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
-            <p className="problem_Question">What is the sum of the digits of the number 21000?</p>
-            <p className="problem_btn">Solution</p>
-          </article>
+          <div className="column">
+            <article className="problem">
+              <h2 className="problem_title">Multiples of 3 and 5</h2>
+              <p className="problem_description">If we list all natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
+              <p className="problem_Question">Find the sum of all the multiples of 3 or 5 below 1000.</p>
+              <p className="problem_btn">Solve</p>
+            </article>
+          </div>
+          <div className="column">
+            <article className="problem">
+              <h2 className="problem_title">Power digit sum</h2>
+              <p className="problem_description">215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
+              <p className="problem_Question">What is the sum of the digits of the number 21000?</p>
+              <p className="problem_btn">Solve</p>
+            </article>
+          </div>
+          <div className="column">
+            <article className="problem">
+              <h2 className="problem_title">Multiples of 3 and 5</h2>
+              <p className="problem_description">If we list all natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
+              <p className="problem_Question">Find the sum of all the multiples of 3 or 5 below 1000.</p>
+              <p className="problem_btn">Solve</p>
+            </article>
+          </div>
+          <div className="column">
+            <article className="problem">
+              <h2 className="problem_title">Power digit sum</h2>
+              <p className="problem_description">215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
+              <p className="problem_Question">What is the sum of the digits of the number 21000?</p>
+              <p className="problem_btn">Solve</p>
+            </article>
+          </div>
         </div>    
       </div>
     );
